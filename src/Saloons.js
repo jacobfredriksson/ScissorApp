b6f15b9f5d564c00866a121d12f51bd4b3e890b4
Jacob Fredriksson <jacobfredriksson@live.se>
2017-09-18 23:24:33 +0200

    image

--- src/Saloons.js
+++ src/Saloons.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './css/Saloons.css';
 import BackButton from './BackButton';
 import BackArrow from './BackArrow';
+import {HashRouter, Link} from 'react-router-dom';
 
 export default class Saloons extends Component {
 
@@ -9,23 +10,43 @@ export default class Saloons extends Component {
 
   render() {
     return (
-      <div>
+      <HashRouter>
+        <div>
 
-        <img className="itopbar" src="https://github.com/jacobfredriksson/ScissorApp/blob/master/Resources/Images/leftarrowbig.png?raw=true"/>
-        <BackArrow />
-        {/* <h1 className="text-center"> Sallonger </h1>
-        <div className="row Price-box">
-          <p className="col-sm-8 price-interval"> Pris 250-500 kr </p>
-        </div>
-        {
-          this.props.salonger.map((salongerna, i) =>
-          <div>
-            <h1> {salongerna.name} </h1>
+          <img className="itopbar" src="https://github.com/jacobfredriksson/ScissorApp/blob/master/Resources/Images/itopbar.png?raw=true"/>
+
+          <img className="heart" src="        https://github.com/jacobfredriksson/ScissorApp/blob/master/Resources/Images/heart.png?raw=true"/>
+
+          <img className="stars" src="https://github.com/jacobfredriksson/ScissorApp/blob/master/Resources/Images/stars.png?raw=true"/>
+
+
+          <BackArrow />
+
+
+          <div className="info-schedule-container">
+            <div className="info-schedule">
+              <Link to="/">Info</Link>
+            </div>
+
+            <div className="info-schedule">
+              <Link to="/">Schema</Link>
+            </div>
           </div>
-          )
-        }  */}
-        <BackButton />
-      </div>
+
+
+          {/* <h1 className="text-center"> Sallonger </h1>
+          <div className="row Price-box">
+            <p className="col-sm-8 price-interval"> Pris 250-500 kr </p>
+          </div> */}
+          {
+            this.props.salonger.map((salongerna, i) =>
+            <div className="">
+              <p className="saloon-name"> {salongerna.name} </p>
+            </div>
+            )
+          }
+        </div>
+      </HashRouter>
     );
   }
 }
